112483af146cebb596fb76bb0b4936415f227f61
Justin Loh <justinlohkingwei@gmail.com>
2022-12-09 14:18:16 -0500

    Sizing issues again

--- src/components/layout/AboutImage.tsx
+++ src/components/layout/AboutImage.tsx
@@ -5,29 +5,29 @@ import { AboutProfile } from "../../data/AboutData";
 const AboutImageRoot = styled.div`
   display: flex;
   height: 37rem;
-  padding: 10% 10% 0 10%;
-  padding-top: 10%;
+  padding-top: 10%; 
   justify-content: center;
   max-width: 100%;
 `;
 
 const AboutImageContainer = styled.div`
   display: flex;
-  min-width: 20rem;
-  width: 45rem;
+  min-width: 5em;
+  width: 30em;
   justify-content: center;
   background-color: black;
   color: #ffbe94;
   opacity: 0.9;
-  max-height: 33rem;
+  max-height: 33em;
   border-radius: 2em;
   padding-top: 2em;
 `;
 
 const AboutImageHolder = styled.div`
   display: block;
+  min-width: 10em ;
   width: 20em;
-  max-height: 25rem;
+  max-height: 25em;
   object-fit: contain;
 `;
 
